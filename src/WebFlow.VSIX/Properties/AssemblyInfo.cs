99ea84a3ef536b377142a8dda4a5adcf6bb068d8
Ackara <ackara.dev@outlook.com>
2018-05-31 12:48:23 -0400

    Increment the version number to '0.0.19'.

--- src/WebFlow.VSIX/Properties/AssemblyInfo.cs
+++ src/WebFlow.VSIX/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebFlow.VSIX")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WebFlow")]
+[assembly: AssemblyDescription("this is a description")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebFlow.VSIX")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Ackara")]
+[assembly: AssemblyProduct("WebFlow")]
+[assembly: AssemblyCopyright("All rigths reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,6 +28,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.0.19")]
+[assembly: AssemblyVersion"0.0.19"")]
+[assembly: AssemblyFileVersion("0.0.19")]
+[assembly: AssemblyInformationalVersion("0.0.19")]
