98df7a58305e15daa8985bbe88ccdf3ac0382211
Ackara <ackara.dev@outlook.com>
2018-07-03 16:05:03 -0400

    Increment the version number to '0.0.29'.

--- src/WebFlow.VSIX/Properties/AssemblyInfo.cs
+++ src/WebFlow.VSIX/Properties/AssemblyInfo.cs
@@ -28,10 +28,11 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.28")]
-[assembly: AssemblyVersion("0.0.28")]
-[assembly: AssemblyFileVersion("0.0.28")]
-[assembly: AssemblyInformationalVersion("0.0.28")]
+// [assembly: AssemblyVersion("0.0.29")]
+[assembly: AssemblyVersion("0.0.29")]
+[assembly: AssemblyFileVersion("0.0.29")]
+[assembly: AssemblyInformationalVersion("0.0.29")]
+
 
 
 
