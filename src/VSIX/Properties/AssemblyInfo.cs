fb9eb48c9db48e0e35e9f4ef38da405772ad9531
Ackara <ackara.dev@outlook.com>
2018-09-08 11:47:25 -0400

    Increment the version number to '0.0.34'.

--- src/VSIX/Properties/AssemblyInfo.cs
+++ src/VSIX/Properties/AssemblyInfo.cs
@@ -28,10 +28,11 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.33")]
-[assembly: AssemblyVersion("0.0.33")]
-[assembly: AssemblyFileVersion("0.0.33")]
-[assembly: AssemblyInformationalVersion("0.0.33")]
+// [assembly: AssemblyVersion("0.0.34")]
+[assembly: AssemblyVersion("0.0.34")]
+[assembly: AssemblyFileVersion("0.0.34")]
+[assembly: AssemblyInformationalVersion("0.0.34")]
+
 
 
 
